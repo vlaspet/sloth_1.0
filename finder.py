762148c2d16e81ba404a458384a9e9fa83004447
vlaspet <vlaspet@Petros-MacBook-Air.local>
2023-01-12 21:46:29 -0800

    Found a bug in text filter end fixed it

--- finder.py
+++ finder.py
@@ -1,33 +1,10 @@
 from text_filter import TextFilter
 from dictionary import Dictionary
 
-# class Finder(TextFilter, Dictionary):
-#     def __init__(self, dict_name, text_name):
-#         Dictionary.__init__(self, dict_name)
-#         TextFilter.__init__(self, text_name)
-#         self.new_words = []
-
-#         dic = self.get_dictionary()
-#         words = self.get_words()
-
-#         for x in words:
-#             if x not in dic:
-#                 self.new_words.append(x)
-
-#     def get_new_words_from_lists(dict, words):
-#         buffer = []
-#         for x in words:
-#             if x not in dict:
-#                 buffer.append(x)
-#         return buffer
-
-#     def get_new_words(self):
-#         return self.new_words
-
 class Finder:
     def __init__(self, dict_names, text_name):
         self.new_words = []
-        
+
         dict_f = []
         for x in dict_names:
             dict_f.extend(Dictionary(x).get_dictionary())
--- text_filter.py
+++ text_filter.py
@@ -3,18 +3,22 @@ import re
 class TextFilter:
     def __init__(self, file):
         self.data = []
-        with open(file, "r") as dict:
+        with open(file, "r", encoding="utf-8") as dict:
             dict_buff = {}
             for line in dict:
                 buffer_line = line
+                # that's filtering text from everything but not
+                # from letters and symbols "'" and '-'
                 buffer_line = re.sub(r"[^A-Za-z'-]", ' ', buffer_line.casefold())
+
+                # splits line on parts by " "
                 buffer = buffer_line.split()
                 if len(buffer) > 1:
                     for word in buffer:
-                        buff = word.strip("-'")
+                        buff = word.strip("-")
                         dict_buff[buff] = None
-                else:
-                    buff = ''.join(filter(str.isalpha, str(buffer)))
+                elif len(buffer) == 1:
+                    buff = buffer[0]
                     dict_buff[buff] = None
             self.data = dict_buff.keys()
             dict.close()
